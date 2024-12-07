74e335e8699415d85969d4540252c51aee218003
eloi-charly <nandry556@gmaial.com>
2024-12-07 14:37:07 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+// declare(strict_types=1);
+
+class Pont{
+    public float $longueur;
+}
+
+
+
+$pont = new Pont;
+
+$pont->longueur = "56.36";
+
+// print_r($pont->longueur);
+$data = 0.1+0.2 === 0.3;
+print_r($data);
